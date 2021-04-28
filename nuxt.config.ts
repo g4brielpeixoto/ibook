9e1a5fed8bf6d98f55396d6ed534c814f720d9e2
bilpeixoto <g4briel.soarespeixoto@gmail.com>
2021-04-28 10:26:22 -0300

    Alteração do favicon e title da página de leitura

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,7 +14,7 @@ export default {
         content: 'Uma nova maneira de ler livros'
       }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
--- pages/book/_id/read.vue
+++ pages/book/_id/read.vue
@@ -4,5 +4,21 @@
 
 <script lang="ts">
 import Vue from 'vue'
-export default Vue.extend({ layout: 'ibook' })
+import { books } from '@/store'
+
+export default Vue.extend({
+  layout: 'ibook',
+  head() {
+    return {
+      title: books.$single.title,
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: books.$single.description
+        }
+      ]
+    }
+  }
+})
 </script>
